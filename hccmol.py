dc5e17ebcf282fe61412079c916ad96f010e2bd5
Alfredo Di Napoli <alfredodinapoli@MacchyPro.local>
2011-06-27 10:35:34 +0200

    Manca solo da sistemare il parser per estendere il tutto alla visualizzazione di proteine grandi.

--- hccmol.py
+++ hccmol.py
@@ -5,17 +5,19 @@ from graphics import *
 from hccsphere import *
 import pstats
 import cProfile
+from math import log
 
 
 class Atom:
-    def __init__(self, name, coords):
+    def __init__(self, name, coords, radius = 3):
         self.name = name.lower().capitalize()
         self.radius = atomic_radius[self.name[0]]
-        self.scale = self.radius[3]/300.0
+        self.scale = self.radius[radius]/300.0
         self.color = atom_color[self.name[0]]
         self.graph = None
         self.coords = coords
         self.batches = None
+        self.volume = None
 
         
     def build(self):
@@ -31,12 +33,24 @@ class Atom:
     def view(self):
         MOLVIEW(self.batches)
 
+        
+    def volview(self):
+        if not self.volume:
+            self.volume = sectionize(self)
+
+        print self.volume
+        MOLVIEW(self.volume)
+
+    def sectionize(self):
+        self.volume = sectionize(self)
+
 
 class Molecule:
     def __init__(self, filename):
         self.filename = filename
         self.atoms = []
         self.batches = []
+        self.volumes = []
 
 
     def build(self):
@@ -58,21 +72,31 @@ class Molecule:
         for a in self.atoms:
             add(a.batches)
 
+    def sectionize(self):
+        for a in self.atoms:
+            a.sectionize()
+            self.volumes.extend(a.volume)
+
     def view(self):
         MOLVIEW(self.batches)
+
+
+    def volview(self):
+        if not self.volumes:
+            for a in self.atoms:
+                a.sectionize()
+                self.volumes.extend(a.volume)
+
+        MOLVIEW(self.volumes)
 
 
 if __name__ == "__main__":
 
     start = time.clock()
-    glu = Molecule("GLU.pdb")
-    cProfile.runctx("glu.build()", globals(), locals(), "molprof")
-    #glu.build()
+    glu = Molecule("3NK2.pdb")
+    glu.build()
     end = time.clock()
-
-    p = pstats.Stats('molprof')
-    p.sort_stats('cumulative')
-    p.print_stats()
 
     print "Molecule builded in ", end - start, " seconds."
     glu.view()
+    glu.volview()
